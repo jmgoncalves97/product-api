15bc7c742603a10e18cdcd8ee9f34c9edd166633
Jonathan Gonçalves <jonathanmattosgoncalves1@gmail.com>
2023-06-09 16:41:09 +0000

    Product feature test added

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->paragraph(),
+            'group_id' => 1,
+            // outras colunas do seu modelo
+        ];
+    }
+}
---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_requisition_for_all_products(): void
+    {
+        $user = User::factory()->create();
+        $products = Product::factory(10)->create();
+    
+        $response = $this->actingAs($user)->get('/api/v1/products/');
+
+        $response->assertStatus(200);
+
+        foreach ($products as $product) {
+            $response->assertSee($product->name);
+        }
+    }
+
+    public function test_product_request_by_id(): void
+    {
+        $user = User::factory()->create();
+        $product = Product::factory()->create();
+    
+        $response = $this->actingAs($user)->get('/api/v1/products/' . $product->id);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonFragment([
+            'id' => $product->id,
+            'name' => $product->name,
+            'description' => $product->description,
+            'group_id' => $product->group_id,
+        ]);
+    }
+
+    public function test_create_new_product(): void
+    {
+        $user = User::factory()->create();
+        $product = Product::factory()->create();
+    
+        $response = $this->actingAs($user)->post('/api/v1/products/', $product->toArray());
+
+        $response->assertStatus(201);
+
+        $response->assertJsonFragment([
+            'name' => $product->name,
+            'description' => $product->description,
+            'group_id' => $product->group_id,
+        ]);
+    }
+
+    public function test_delete_product(): void
+    {
+        $user = User::factory()->create();
+        $product = Product::factory()->create();
+    
+        $response = $this->actingAs($user)->delete('/api/v1/products/' . $product->id);
+
+        $response->assertStatus(204);
+    }
+}
