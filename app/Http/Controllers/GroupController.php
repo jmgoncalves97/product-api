7c189885e5ee8ba6628e4ccfef098815063fbba6
Jonathan Gonçalves <jonathanmattosgoncalves1@gmail.com>
2023-06-09 14:52:32 +0000

    Group endpoint creeated

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group;
+use Illuminate\Http\Client\HttpClientException;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function __construct()
+    {
+        $this->M = Group::class;
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $group = new Group;
+            $group->name = $request->input('name');
+            $group->description = $request->input('description');
+            $group->save();
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 400);
+        }
+        return response()->json($group);
+    }
+}
