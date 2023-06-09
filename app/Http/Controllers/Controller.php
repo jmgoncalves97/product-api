2f0aa061c0518387bd877507a32ff2803d39a41b
Jonathan Gonçalves <jonathanmattosgoncalves1@gmail.com>
2023-06-09 14:55:32 +0000

    Controller parent modified

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected $M;
+
+    public function index()
+    {
+        $collection = $this->M::all();
+        return response()->json($collection);
+    }
+
+    public function show($id)
+    {
+        $record = $this->M::find($id);
+        if (!$record) {
+            return response()->json(['message' => 'Não encontrado'], 404);
+        }
+        return response()->json($record);
+    }
+     
+    public function destroy($id)
+    {
+        $model = $this->M::find($id);
+        if (!$model) {
+            return response()->json(['message' => 'Não encontrado'], 404);
+        }
+        $model->delete();
+        return response()->json(['message' => 'Removido com sucesso']);
+    }
 }
