72637d4b180057169e626861412a0a409daedc9b
Jonathan Gonçalves <jonathanmattosgoncalves1@gmail.com>
2024-02-24 19:02:00 -0300

    fix: Illuminate\Support\Facades\Route imported into app/Modules/Plataform1/routes.php

--- app/Modules/Plataform1/routes.php
+++ app/Modules/Plataform1/routes.php
@@ -2,6 +2,7 @@
 
 use App\Modules\Plataform1\Http\Controllers\ProductController;
 use App\Modules\Plataform1\Http\Middleware\EnsureTokenIsValid;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('plataform1/api/v1')->middleware([EnsureTokenIsValid::class])->group(function () {
     Route::get('/products', [ProductController::class, 'index']);
